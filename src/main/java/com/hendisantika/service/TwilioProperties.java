ebb88aa4ba433c71aca537c6b58d35b838985734
Hendi Santika <hendisantika@yahoo.co.id>
2022-03-12 19:00:17 +0700

    #5 - Add TwilioProperties

---
+++ src/main/java/com/hendisantika/service/TwilioProperties.java
@@ -0,0 +1,24 @@
+package com.hendisantika.service;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-twilio-sms-demo2
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/03/22
+ * Time: 18.59
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@Configuration
+@ConfigurationProperties("twilio")
+public class TwilioProperties {
+    private String accountSid;
+    private String authToken;
+    private String fromNumber;
+}
