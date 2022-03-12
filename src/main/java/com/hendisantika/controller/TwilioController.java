3aa8fa8517a7e4c90cdac1b5a4c2e16eb833640e
Hendi Santika <hendisantika@yahoo.co.id>
2022-03-12 19:08:06 +0700

    #11 - Add homePage in TwilioController

--- src/main/java/com/hendisantika/controller/TwilioController.java
+++ src/main/java/com/hendisantika/controller/TwilioController.java
@@ -3,6 +3,8 @@ package com.hendisantika.controller;
 import com.hendisantika.service.SmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,8 +17,14 @@ import org.springframework.stereotype.Controller;
  * To change this template use File | Settings | File Templates.
  */
 @Controller
+@RequestMapping("/")
 public class TwilioController {
 
     @Autowired
     private SmsService smsService;
+
+    @GetMapping
+    public String homePage() {
+        return "index";
+    }
 }
